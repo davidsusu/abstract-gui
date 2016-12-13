8911a47d7bf06d87ae36880e07be65a13508738c
horvath <horvath@patronet.net>
2016-12-13 16:58:25 +0100

    Format support added to i18n

---
+++ abstract-gui/src/hu/webarticum/abstract_gui/framework/i18n/FormatHtmlContent.java
@@ -0,0 +1,44 @@
+package hu.webarticum.abstract_gui.framework.i18n;
+
+import hu.webarticum.abstract_gui.framework.HtmlContent;
+import hu.webarticum.abstract_gui.framework.TextualContent;
+
+public class FormatHtmlContent implements TextualContent {
+    
+    final private TextualContent formatContent;
+    
+    final private Object[] valueContents;
+
+    public FormatHtmlContent(String formatString, Object... valueContents) {
+        this(new HtmlContent(formatString), valueContents);
+    }
+    
+    public FormatHtmlContent(TextualContent formatContent, Object... valueContents) {
+        this.formatContent = formatContent;
+        this.valueContents = valueContents;
+    }
+
+    @Override
+    public String toHtml() {
+        Object[] manipulatedValueContents = new Object[valueContents.length];
+        for (int i = 0; i < valueContents.length; i++) {
+            Object item = valueContents[i];
+            if (item instanceof TextualContent) {
+                item = ((TextualContent)item).toHtml();
+            }
+            manipulatedValueContents[i] = item;
+        }
+        return String.format(formatContent.toHtml(), manipulatedValueContents);
+    }
+
+    @Override
+    public String toString() {
+        return HtmlContent.htmlToString(toHtml());
+    }
+
+    @Override
+    public boolean isPlain() {
+        return true;
+    }
+
+}
